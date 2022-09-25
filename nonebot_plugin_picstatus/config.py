d13f534dd43c9ba5ee4572a89de7cd04dbbe40fc
student_2333 <lgc2333@126.com>
2022-09-26 01:01:57 +0800

    fix type

--- nonebot_plugin_picstatus/config.py
+++ nonebot_plugin_picstatus/config.py
@@ -11,9 +11,9 @@ class Cfg(BaseModel):
     ps_only_su: bool = False
     ps_blur_radius: int = 4
     ps_font: Optional[str] = None
-    ps_ignore_parts: List[str] = set()
+    ps_ignore_parts: List[str] = []
     ps_ignore_bad_parts: bool = False
-    ps_ignore_disk_ios: List[str] = set()
+    ps_ignore_disk_ios: List[str] = []
     ps_use_env_nick: bool = False
     ps_need_at: bool = False
     ps_mask_color: Tuple[int, int, int, int] = (255, 255, 255, 125)
